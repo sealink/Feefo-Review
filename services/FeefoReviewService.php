a64aa337e3bac85ed39373a307a22ff744c53124
Mina Kim <mina.kim@sealink.com.au>
2018-07-12 15:51:37 +0930

    Change a function to get products

--- services/FeefoReviewService.php
+++ services/FeefoReviewService.php
@@ -28,15 +28,15 @@ class FeefoReviewService extends BaseApplicationComponent
   }
 
   private function getProducts() {
-    $cacheKey = 'feefo_'.$this->feefoMerchantId();
-    $products = craft()->cache->get($cacheKey);
-    if (!is_null($products)) {
-      $products = $this->getProductReviews();
-    } else {
+  $cacheKey = 'feefo_'.$this->feefoMerchantId();
+  $products = craft()->cache->get($cacheKey);
+  if (is_null($products)) {
+    $products = $this->getProductReviews();
+    if (!is_null($products)
       craft()->cache->set($cacheKey, $products, 24*3600 /*24 hours*/);
-    }
-    return $products;
   }
+  return $products;
+}
 
   private function getProductReviews()
   {
