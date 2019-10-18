eb4eb423e6e6919d39a5da3e0d983735f32bf612
deanvollebregt <deanvollebregt@deanvollebregts-MacBook-Pro.local>
2019-10-18 14:27:43 +1030

    Fix Feefo extension crashing sites when unavailable
    Fix Feefo extension crashing sites when unavailable
    
    Fix Feefo extension crashing sites when unavailable
    
    Fix Feefo extension crashing sites when unavailable
    
    Fix Feefo extension crashing sites when unavailable
    
    Fix Feefo extension crashing sites when unavailable

--- services/FeefoReviewService.php
+++ services/FeefoReviewService.php
@@ -1,26 +1,24 @@
 <?php
 namespace Craft;
 
-class FeefoReviewService extends BaseApplicationComponent
-{
+class FeefoReviewService extends BaseApplicationComponent {
   public $plugin = null;
   public $settings = array();
 
-  public function __construct()
-  {
+  public function __construct(){
     $this->plugin   = craft()->plugins->getPlugin('feeforeview');
     $this->settings = $this->plugin->getSettings();
   }
 
-  public function getReviewSummary($id)
-  {
-    if ($this->feefoMerchantId() == null) return null;
-
+  public function getReviewSummary($id){
+    $merchantId = $this->feefoMerchantId();
     $products = $this->getProducts();
-    if ($products == null) return null;
-
-    $index = array_search($id, array_column($products, 'vendor_ref'));
-    return $index !== false ? $products[$index] : null;
+    if (is_null($merchantId) || is_null($products)) {
+      return null;
+    } else {
+      $index = array_search($id, array_column($products, 'vendor_ref'));
+      return $index !== false ? $products[$index] : null;
+    }
   }
 
   public function feefoMerchantId() {
@@ -29,19 +27,22 @@ class FeefoReviewService extends BaseApplicationComponent
 
   private function getProducts() {
     $cacheKey = 'feefo_'.$this->feefoMerchantId();
-    $products = craft()->cache->get($cacheKey);
-    if ($products === false) {
-      $products = $this->getProductReviews();
-      if (!is_null($products))
-        craft()->cache->set($cacheKey, $products, 24*3600 /*24 hours*/);
+    $products = $this->getProductReviews();
+    if (!is_null($products)) {
+      craft()->cache->set($cacheKey, $products, 24*3600 /*24 hours*/);
+      return $products;
+    } else {
+      return null;
     }
-    return $products;
   }
 
-  private function getProductReviews()
-  {
-    $data = file_get_contents('https://api.feefo.com/api/10/products/ratings?merchant_identifier='.$this->feefoMerchantId().'&review_count=true&since_period=year');
-    $decodedData = json_decode($data);
-    return $decodedData->products;
+  private function getProductReviews() {
+    $data = @file_get_contents('https://api.feefo.com/api/10/products/ratings?merchant_identifier='.$this->feefoMerchantId().'&review_count=true&since_period=year');
+    $decodedData = @json_decode($data);
+    if(isset($decodedData) && isset($decodedData->products)){
+      return $decodedData->products;
+    } else {
+      return null;
+    }
   }
 }
