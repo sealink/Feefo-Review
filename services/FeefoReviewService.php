23e5083baf896d6ecc47273d9d00c3d23a305480
deanvollebregt <deanvollebregt@deanvollebregts-MacBook-Pro.local>
2019-10-22 13:56:51 +1030

    apply fixes from code review

--- services/FeefoReviewService.php
+++ services/FeefoReviewService.php
@@ -5,20 +5,17 @@ class FeefoReviewService extends BaseApplicationComponent {
   public $plugin = null;
   public $settings = array();
 
-  public function __construct(){
+  public function __construct() {
     $this->plugin   = craft()->plugins->getPlugin('feeforeview');
     $this->settings = $this->plugin->getSettings();
   }
 
-  public function getReviewSummary($id){
-    $merchantId = $this->feefoMerchantId();
+  public function getReviewSummary($id) {
+    if (is_null($this->feefoMerchantId())) return null;
+
     $products = $this->getProducts();
-    if (is_null($merchantId) || is_null($products)) {
-      return null;
-    } else {
-      $index = array_search($id, array_column($products, 'vendor_ref'));
-      return $index !== false ? $products[$index] : null;
-    }
+    $index = array_search($id, array_column($products, 'vendor_ref'));
+    return $index !== false ? $products[$index] : null;
   }
 
   public function feefoMerchantId() {
@@ -27,22 +24,21 @@ class FeefoReviewService extends BaseApplicationComponent {
 
   private function getProducts() {
     $cacheKey = 'feefo_'.$this->feefoMerchantId();
-    $products = $this->getProductReviews();
-    if (!is_null($products)) {
+    $products = craft()->cache->get($cacheKey);
+    if ($products === false) {
+      $products = $this->getProductReviews();
       craft()->cache->set($cacheKey, $products, 24*3600 /*24 hours*/);
-      return $products;
-    } else {
-      return null;
     }
+    return $products;
   }
 
   private function getProductReviews() {
     $data = @file_get_contents('https://api.feefo.com/api/10/products/ratings?merchant_identifier='.$this->feefoMerchantId().'&review_count=true&since_period=year');
     $decodedData = @json_decode($data);
-    if(isset($decodedData) && isset($decodedData->products)){
+    if (isset($decodedData) && isset($decodedData->products)) {
       return $decodedData->products;
     } else {
-      return null;
+      return [];
     }
   }
 }
